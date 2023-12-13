5ee1cfa63dd9880d776840d3db06a9cb1ccadb8a
tcisio10 <almeida.tarcisio086@gmail.com>
2023-12-13 20:14:23 -0300

    aoc update

--- floorshare.js
+++ floorshare.js
@@ -8,6 +8,9 @@ function calcular(){
   var tcl = document.getElementById("tcl")
   var toshiba = document.getElementById("toshiba")
   var vizzion = document.getElementById("vizzion")
+  var philips = document.getElementById("philips")
+  var aiwa = document.getElementById("aiwa")
+  var aoc = document.getElementById('aoc')
   var resultado = document.getElementById("resultado")
 
   if(lg.value.length == " "){
@@ -23,8 +26,17 @@ function calcular(){
   if(vizzion.value.length == " "){
     vizzion.value = 0
   }
+  if(philips.value.length == " "){
+    philips.value = 0
+  }
+  if(aiwa.value.length == " "){
+    aiwa.value = 0
+  }
+  if (aoc.value.length == " "){
+    aoc.value = 0
+  }
 
-  var total = Number(lg.value) + Number(ssg.value) + Number(philco.value) + Number(tcl.value) + Number(toshiba.value) + Number(vizzion.value)
+  var total = Number(lg.value) + Number(ssg.value) + Number(philco.value) + Number(tcl.value) + Number(toshiba.value) + Number(vizzion.value) + Number(philips.value) + Number(aiwa.value) + Number(aoc.value)
 
   lgResultado = (lg.value*100) / total
   ssgResultado = (ssg.value*100) / total
@@ -32,10 +44,28 @@ function calcular(){
   tclResultado = (tcl.value*100) / total
   toshibaResultado = (toshiba.value*100) / total
   vizzionResultado = (vizzion.value*100) / total
+  philipsResultado = (philips.value*100) / total
+  aiwaResultado = (aiwa.value*100) / total
+  aocResultado = (aoc.value*100) / total
+
+  totalPorcetagem = lgResultado + ssgResultado + philcoResultado + tclResultado+toshibaResultado + vizzionResultado + philipsResultado + aiwaResultado + aocResultado
 
  
  
-    resultado.innerHTML = `TCL: ${tclResultado.toFixed(2)}% -  ${tcl.value} Peças</br>Samsung: ${ssgResultado.toFixed(2)}% -  ${ssg.value} Peças</br>Philco: ${philcoResultado.toFixed(2)}% -  ${philco.value} Peças</br>LG: ${lgResultado.toFixed(2)}% -  ${lg.value} Peças</br>Vizzion: ${vizzionResultado.toFixed(2)}% -  ${vizzion.value} Peças</br>Toshiba: ${toshibaResultado.toFixed(2)}% - ${toshiba.value} Peças</br>Total ${total} peças`
+    resultado.innerHTML = 
+    `TCL: ${tclResultado.toFixed(2)}% -  ${tcl.value} Peças</br>
+    Samsung: ${ssgResultado.toFixed(2)}% -  ${ssg.value} Peças</br>
+    LG: ${lgResultado.toFixed(2)}% -  ${lg.value} Peças</br>
+    Philips: ${philipsResultado.toFixed(2)}% - ${philips.value} Peças</br>
+    Philco: ${philcoResultado.toFixed(2)}% -  ${philco.value} Peças</br>
+    Toshiba: ${toshibaResultado.toFixed(2)}% - ${toshiba.value} Peças</br>
+    AOC: ${aocResultado.toFixed(2)}% - ${aoc.value} Peças</br>
+    Aiwa: ${aiwaResultado.toFixed(2)}% - ${aiwa.value} Peças</br>
+    Vizzion: ${vizzionResultado.toFixed(2)}% -  ${vizzion.value} Peças</br>
+    
+    
+    
+    Total ${total} peças | ${totalPorcetagem.toFixed(2)}%`
 
     const ctx = document.getElementById('myChart');
     
@@ -43,12 +73,12 @@ function calcular(){
       type: 'pie',
       
       data: {
-        labels: ['TCL', 'SAMSUNG', 'PHILCO', 'LG', 'TOSHIBA','VIZZION'],
+        labels: ['TCL', 'SAMSUNG', 'PHILCO', 'LG', 'TOSHIBA','Philips','VIZZION','AIWA', 'AOC'],
         
         datasets: [{
           label: '# QTD TVs',
-          data: [tclResultado, ssgResultado, philcoResultado, lgResultado, toshibaResultado, vizzionResultado],
-          backgroundColor: ["#d00000", "#4361ee", "#293241", "#ff5a5f","#f77f00", "#ffd60a"],
+          data: [tclResultado, ssgResultado, philcoResultado, lgResultado, toshibaResultado,philipsResultado, vizzionResultado, aiwaResultado, aocResultado],
+          backgroundColor: ["#d00000", "#4361ee", "#293241", "#ff5a5f","#f77f00","#00009C", "#ffd60a", "#993399", "#d3d3d3 "],
           borderWidth: 1
         }]
       },
